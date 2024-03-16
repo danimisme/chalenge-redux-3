c0b95e0cc48a618ed77c4a7641a77100fafc0e05
danimisme <msubhanr53@gmail.com>
2024-03-16 21:01:43 +0700

    add authReducer to store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from "redux";
 import counterReducer from "./features/counter/counterSlice";
+import authReducer from "./features/auth/authSlice";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  auth: authReducer,
 });
 
 const store = createStore(rootReducer);
